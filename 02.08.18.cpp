2302f0c7400b42204ee1b1449d3fd0754cfa10b6
samtheteddybear <sbpiyal@gmail.com>
2018-02-08 11:13:39 -0500

    Update 02.08.18.cpp

--- 02.08.18.cpp
+++ 02.08.18.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using std::string;
+using namespace std; 
 
+int main(){
+	
+}
+
+
+
+
+
+/* NOTES: 
+- 	"Brute force" for GCD( greatest common divisor)
+scd(a,b) = max ( d | and d |b) 
+		   d E z
+		        "there exists" 
+		        
